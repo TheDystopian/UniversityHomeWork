d89878b30e15dad35fbb3ffdce5fa02e01ca6404
TheDistopian <mrgsemail007@gmail.com>
2021-12-05 01:07:54 +0300

    If statement reduction

--- HomeWork Balls.cpp
+++ HomeWork Balls.cpp
@@ -11,32 +11,24 @@ int factor(int f) {
 
 int perestanovka(vector<bool> balls,int n, int m = 1) {
     int counter = 0;
-    if (m <= n) {
-        for (int i = 0; i < n; i++) {
-            if (!balls[i]) {
-                if (i == m) counter += factor(n - m);
-                else {
-                    balls[i] = true;
-                    counter += perestanovka(balls, n, m+1);
-                    balls[i] = false;
-                }
-            }
+    for (int i = 0; i < n && m <= n; i++) {
+        if (balls[i]) continue;
+        if (i == m) counter += factor(n - m);
+        else {
+            balls[i] = true;
+            counter += perestanovka(balls, n, m + 1);
+            balls[i] = false;
         }
     }
     return counter;
 }
 
-
-void Balls() {
-    int n;
-    cout << "I've got balls of steel\n- Duke Nukem\nА если серьезно, каково количество шариков: ";
-    if (!(cin >> n) || n < 1) { cout << "Ну че теперь, пиши заявление в полицию о краже шаров и после возврата перезапусти\n"; return;}
-
-    cout << "Количество перестановок: " << perestanovka(*new vector <bool>(n), n) << '\n';
-}
-
 int main() {
     setlocale(0, "");
 
-    Balls();
+    int n;
+    cout << "I've got balls of steel\n- Duke Nukem\nА если серьезно, каково количество шариков: ";
+    if (!(cin >> n) || n < 1) { cout << "Ну че теперь, пиши заявление в полицию о краже шаров и после возврата перезапусти\n"; return 1; }
+
+    cout << "Количество перестановок: " << perestanovka(*new vector <bool>(n, false), n) << '\n';
 }
